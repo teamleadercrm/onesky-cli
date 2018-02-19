51b81b396c4b00ed0ad7cb6a81e774bc1b307e56
marijn vandevoorde <marijn@teamleader.eu>
2018-02-19 15:13:00 +0100

    Linting™

--- src/DownloadCommand.php
+++ src/DownloadCommand.php
@@ -10,7 +10,7 @@ class DownloadCommand extends Command
 {
     protected function configure()
     {
-		parent::configure();
+        parent::configure();
         $this
             ->setName('download')
             ->setDescription('Download a translation file from OneSky')
@@ -21,32 +21,31 @@ class DownloadCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-		$this->initializeClient($input->getOption('key'), $input->getOption('secret'));
+        $this->initializeClient($input->getOption('key'), $input->getOption('secret'));
 
-		$project_id = $input->getOption('project_id');
-		if (is_null($project_id)) {
+        $project_id = $input->getOption('project_id');
+        if (is_null($project_id)) {
             $project_id = $this->config['project_id'];
-
         }
 
         $response = $this->client->translations('export', [
-            'project_id' => (int) $project_id,
+            'project_id' => (int)$project_id,
             'locale' => $input->getArgument('locale'),
             'source_file_name' => $input->getArgument('source_file_name'),
         ]);
 
-        if (! $response) {
+        if (!$response) {
             $output->writeln('<error>Empty OneSky response!</error>');
             return;
         }
 
         $data = json_decode($response, true);
         if (isset($data['meta']) && $data['meta']['status'] != 201) {
-            $output->writeln('<error>'.$data['meta']['message'].'</error>');
+            $output->writeln('<error>' . $data['meta']['message'] . '</error>');
             return;
         }
 
         file_put_contents($input->getArgument('file'), $response);
-        $output->writeln('<info>Created '.$input->getArgument('file').'</info>');
+        $output->writeln('<info>Created ' . $input->getArgument('file') . '</info>');
     }
 }
